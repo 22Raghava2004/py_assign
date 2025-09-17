801ed04925412ac133f7660dadec55d8b6b31d73
22Raghava2004 <raghavangowda2004@gmail.com>
2025-09-17 21:39:48 +0530

    go on

---
+++ assign_1.py
@@ -0,0 +1,20 @@
+
+num1=float(input("enter the first number: "))
+num2=float(input("input th second number: "))
+operation=input("enter the operation: ")
+if operation=="+":
+    add=num1+num2
+    print(f"the addition of num1 and num2 is{add}")
+elif operation=="-":
+    sub=num1-num2
+    print(f"the subtraction of num1 and num2 is {sub}")
+elif operation=="*":
+    mul=num1*num2
+    print(f"the multiplication of num1 and num2 is {mul}")
+elif operation=="/":
+    if num2==0:
+        print("division by zero is not allowed")
+    else:
+        div=num1/num2
+        print(f"the division of num1 and num2 is {div}")
+
---
+++ greeting.py
@@ -0,0 +1,3 @@
+fir_name=input("Enter your first name: ")
+last_name=input("Enter your last name: ")
+print(f"hello {fir_name+last_name} welcome yo to the python programming")
