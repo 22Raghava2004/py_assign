0c95507942a29724208142179c4669e59f833631
22Raghava2004 <raghavangowda2004@gmail.com>
2025-10-02 20:47:59 +0530

    assignment3

---
+++ assign3/ass3_2.py
@@ -0,0 +1,8 @@
+import math
+num=int(input("enter the number:"))
+squre_root=math.sqrt(num)
+print(f"the squre root of {num} is {squre_root}")
+logrithm=math.log(num)
+print(f"the logrithm of {num} is {logrithm}")
+sine=math.sin(num)
+print(f"the sine of {num} is {sine}") #this is in radiansv
